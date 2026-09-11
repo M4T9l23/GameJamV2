ba86e2a793d21bfbf956e814efb3e6d1a55720cf
M4T9l23 <mirajira8@gmail.com>
2026-09-11 19:46:15 +0200

    basick firing

--- Player/Player.cs
+++ Player/Player.cs
@@ -1,36 +1,44 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-	
-	public float Speed = 400f;
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		
-	}
-	public override void _PhysicsProcess(double delta)
-	{
-			Vector2 velocity = Velocity;
-     
-			float dir = Input.GetAxis("ui_left", "ui_right");
-			velocity.X = dir * Speed;
-         
-			float dir_y = Input.GetAxis("ui_up", "ui_down");
-			velocity.Y = dir_y * Speed;
-         
-			Velocity = velocity;
-			MoveAndSlide();
-     
-			// Debug log to confirm movement
-			if (velocity != Vector2.Zero)
-			{
-				GD.Print("Player Position: ", GlobalPosition);
-			}
-		}
+    private const float Speed = 200f;
+
+    [Export] public PackedScene BulletScene;
+    [Export] public float FireRate = 1.00f;
+
+    private Vector2 _facing = Vector2.Right;
+    private bool _canShoot = true;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 input = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        Velocity = input * Speed;
+        if (Velocity != Vector2.Zero)
+        {
+            GD.Print("Player Position", GlobalPosition);
+        }
+        if (input != Vector2.Zero)
+            _facing = input.Normalized();
+
+        MoveAndSlide();
+
+        if (Input.IsActionPressed("shoot") && _canShoot)
+            Shoot();
+    }
+
+    private async void Shoot()
+    {
+        _canShoot = false;
+
+        var bullet = BulletScene.Instantiate<Attack1>();
+        bullet.Direction = _facing;
+        bullet.Rotation = _facing.Angle();
+        bullet.Shooter = this;
+        GetTree().CurrentScene.AddChild(bullet);
+        bullet.GlobalPosition = GlobalPosition;
+
+        await ToSignal(GetTree().CreateTimer(FireRate), SceneTreeTimer.SignalName.Timeout);
+        _canShoot = true;
+    }
 }
